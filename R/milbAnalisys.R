98ed93f0395a51c3edf4836f6c88bdadc17004b8
rodrigomotoyama <rodrigo.motoyama@gmail.com>
2020-05-09 01:27:10 -0300

    Código manipulando dados da milb para arremessadores, e visualização dos pitchas brasileiros

--- R/milbAnalisys.R
+++ R/milbAnalisys.R
@@ -10,8 +10,23 @@ milb_data <- map(path, read_csv) %>%
   clean_names() %>%
   select(-ends_with(c("1", "2")))
 milb_data %>%
-  filter(name %in% c("Bo Takahashi", "Daniel Missaki")) %>%
-  select(season, name, team, age, ip, k_percent, bb_percent, era, fip, babip, sw_str_percent) %>%
-  arrange(desc(k_percent)) %>%
+  filter(name %in% c("Bo Takahashi", "Daniel Missaki",
+                     "Igor Januario", "Heitor Tokar",
+                     "Eric Pardinho", "Andre Rienzo")) %>%
+  select(season, name, team, age, ip, k_percent, bb_percent, era, fip, babip, sw_str_percent, w, l) %>%
+  group_by(nome = name, season) %>%
+  summarise(total_vitoria = sum(w), total_derrota = sum(l), "Innings arremessados" = sum(ip)) %>%
   ggplot() +
-  geom_point(aes(x = season, y = sw_str_percent, color = name))
+  geom_point(aes(x = total_derrota, y = total_vitoria,
+                 size = `Innings arremessados`,
+                 colour = nome))+
+  xlab("Derrotas")+ylab("Vitorias")+
+  geom_text(aes(x = total_derrota, y = total_vitoria, label=season))+
+  ylim(0, 11)+
+  xlim(0, 11)+
+  theme(
+    legend.position = "bottom",
+    legend.box = "vertical")+
+  ggtitle("Total de vitorias/derrotas por temporada")
+
+
