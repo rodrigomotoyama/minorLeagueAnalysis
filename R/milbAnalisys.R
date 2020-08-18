4f480265de343348d6661cc093db71449d3ede4e
rodrigomotoyama <rodrigo.motoyama@gmail.com>
2020-08-18 01:07:02 -0300

    nada

--- R/milbAnalisys.R
+++ R/milbAnalisys.R
@@ -1,14 +1,15 @@
 library(tidyverse)
-library(dplyr)
-library(readr)
+
 library(janitor)
-library(ggplot2)
+
 
 path <- map(1:3, ~str_c("data/fangraphs-minor-league-leaders (", .x, ").csv"))
 milb_data <- map(path, read_csv) %>%
-  bind_cols() %>%
+  reduce(full_join) %>%
   clean_names() %>%
   select(-ends_with(c("1", "2")))
+
+
 milb_data %>%
   filter(name %in% c("Bo Takahashi", "Daniel Missaki",
                      "Igor Januario", "Heitor Tokar",
